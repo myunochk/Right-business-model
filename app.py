5ebd7ec1b9ddbbcc80535b36167daf68abc2fad3
Yunochkin <maxsson9@gmail.com>
2020-05-10 12:22:00 +0300

    Library version updated. OptimalParreto function updated (does not use global variables). Each Pareto Optimal Set in html.Details (tag specifies additional details that the user can view or hide on demand) Updated table generation. Moved to separate element rendering functions. Added support for files with the resolution 'txt' or 'tsv' (not tested). Temporarily added dropdown. Tested Pattern-Matching Callbacks (for dash == 1.11). ToggleSwitch temporarily added for testing.

--- app.py
+++ app.py
@@ -2,14 +2,16 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output, State
+import dash_daq as daq # ToggleSwitch
+from dash.dependencies import Input, Output, State, ALL
 import pandas as pd
-import csv
 import numpy as np
 import dash_table
-import datetime
 import io
 import base64
+import random
+from datetime import datetime
+
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
@@ -18,124 +20,46 @@ colors = {
     'background': '#fff',
     'text': '#111'
 }
-def generate_table(dataframe):
-    return html.Table([
-        html.Thead(
-            html.Tr([html.Th(col) for col in dataframe.columns])
-        ),
-        html.Tbody([
-            html.Tr([
-                html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
-            ]) for i in range(min(len(dataframe)))
-        ])
-    ])
+def generate_graph(dataframe):
+    colorss=[]
 
-#app.scripts.config.serve_locally = True
-#app.config.suppress_callback_exceptions = True
-#app.server.config.suppress_callback_exceptions = True
-#app.config[‘suppress_callback_exceptions’] = True
+    random.seed(datetime.now().time())
+    for i in range(len(dataframe)):
+        colorss.append('rgb('
+                       f'{(dataframe["x"][i] * dataframe["y"][i])%random.randint(0,255)},'
+                       f'{(dataframe["x"][i] * dataframe["y"][i])%random.randint(0,255)},'
+                       f'{(dataframe["x"][i] * dataframe["y"][i])%random.randint(0,255)})')
 
-with open("1.csv", 'r') as f:
-    reader = csv.reader(f, delimiter=',')
-    headers = next(reader)
-    data = np.array(list(reader)).astype(float)
-
-app.layout = html.Div(children=(
-    dcc.Upload(
-        id='upload-data',
-        children=html.Div([
-            'Drag and Drop or ',
-            html.A('Select Files')
-        ]),
-        style={
-            'width': '50%',
-            'height': '60px',
-            'lineHeight': '60px',
-            'borderWidth': '1px',
-            'borderStyle': 'dashed',
-            'borderRadius': '5px',
-            'textAlign': 'center',
-            'margin': '10px'
-        },
-        # Allow multiple files to be uploaded
-        multiple=True
-    ),
-    html.Div(id='output-data-upload'),
-    html.Div(id='output-data-pareto'),
-    html.H1(
-        children='Hello Dash',
-        style={
-            'textAlign': 'center',
-            'color': colors['text']
-        }
-    ),
-    dcc.Graph(
-        id='my-graph',
-        figure=dict(
-            data=[
+    return dcc.Graph(
+            figure=dict(
+                data=[
                 dict(
-                    x=data[:, 0],
-                    y=data[:, 1],
+                    x=dataframe['x'],
+                    y=dataframe['y'],
                     name='Rest of world',
-                    mode='markers',
+                    mode= 'markers',
                     marker=dict(
-                        color='rgb(55, 83, 109)',
-                        line={'width': 0.5, 'color': 'white'}
+                        #color={'rgb(55, 83, 109)' if dataframe['x'][i]> 50 else 'rgb(15, 13, 19)' for i in range(len(dataframe['x']))},
+                        color =colorss,
+                        line= {'width': 0.5, 'color': 'white'},
+                        size=abs(dataframe[list(dataframe)[-1]])/2,
                     ),
-                )
+                ),
             ],
-            layout={
-                'plot_bgcolor': colors['background'],
-                'paper_bgcolor': colors['background'],
-                'font': {
-                    'color': colors['text']
+                layout={
+                    'plot_bgcolor': colors['background'],
+                    'paper_bgcolor': colors['background'],
+                    'font': {
+                        'color': colors['text']
+                    },
                 },
-            },
-        ),
-    ),
-    html.Div(children='Dash: A web application framework for Python.', style={
-        'textAlign': 'center',
-        'color': colors['text']
-    })
-))
+            ),
+        )
 
-pre_style = {
-    'whiteSpace': 'pre-wrap',
-    'wordBreak': 'break-all',
-    'whiteSpace': 'normal'
-}
-Pareto = []
-paretoListDiv = []
-def parse_contents(contents, filename):
-    content_type, content_string = contents.split(',')
-    decoded = base64.b64decode(content_string)
-    try:
-        if 'csv' in filename:
-            # Assume that the user uploaded a CSV file
-            df = pd.read_csv(
-                io.StringIO(decoded.decode('utf-8')))
-        elif 'xls' in filename:
-            # Assume that the user uploaded an excel file
-            df = pd.read_excel(io.BytesIO(decoded))
-    except Exception as e:
-        print(e)
-        return html.Div([
-            'There was an error processing this file.'
-        ])
-    Pareto.clear()
-    paretoListDiv.clear()
-    Pareto.append(optimalPareto(df.to_numpy())),
-    OutPareto = printPareto(Pareto)
-    #print(type(Pareto1))
-    #a = dcc.Markdown([str(i[m]) for i in Pareto1.to_dict('records') for m in Pareto1])
-    #a = dcc.Markdown([m+str(i[m]) for i in df.to_dict('records') for m in df])
-    return html.Div([
-        OutPareto,
-        #dcc.Markdown(children=[str(i['y']) for i in Pareto1.to_dict('records')]),
-        #dcc.Markdown(children=[str(i['y']) for i in df.to_dict('records')]),
-        dash_table.DataTable(
-            data=df.to_dict('records'),
-            columns=[{'name': i, 'id': i} for i in df.columns],
+def generate_table(dataframe):
+    return dash_table.DataTable(
+            data=dataframe.to_dict('records'),
+            columns=[{'name': i, 'id': i} for i in dataframe.columns],
             #editable=True,
             style_cell={
                 'textAlign': 'left',
@@ -159,32 +83,130 @@ def parse_contents(contents, filename):
                 'overflowY': 'scroll',
                 'border': 'thin lightgrey solid'
             },
-            ),
-        dcc.Graph(
-            figure=dict(
-                data=[
-                dict(
-                    x=df['x'],
-                    y=df['y'],
-                    name='Rest of world',
-                    mode= 'markers',
-                    marker=dict(
-                        color='rgb(55, 83, 109)',
-                        line= {'width': 0.5, 'color': 'white'},
-                        size=abs(df[list(df)[-1]])/2,
-                    ),
-                )
-            ],
-                layout={
-                    'plot_bgcolor': colors['background'],
-                    'paper_bgcolor': colors['background'],
-                    'font': {
-                        'color': colors['text']
-                    },
-                },
-            ),
-        ),
+            )
+
+def parse_dataa(contents, filename):
+    content_type, content_string = contents.split(',')
+
+    decoded = base64.b64decode(content_string)
+    try:
+        if 'csv' in filename:
+            # Assume that the user uploaded a CSV or TXT file
+            df = pd.read_csv(
+                io.StringIO(decoded.decode('utf-8')))
+        elif 'xls' in filename:
+            # Assume that the user uploaded an excel file
+            df = pd.read_excel(io.BytesIO(decoded))
+        elif 'txt' or 'tsv' in filename:
+            # Assume that the user upl, delimiter = r'\s+'oaded an excel file
+            df = pd.read_csv(
+                io.StringIO(decoded.decode('utf-8')), delimiter = r'\s+')
+    except Exception as e:
+        print(e)
+        return html.Div([
+            'There was an error processing this file.'
+        ])
+
+    return df
+
+#app.scripts.config.serve_locally = True
+app.config.suppress_callback_exceptions = True
+#app.server.config.suppress_callback_exceptions = True
+#app.config[‘suppress_callback_exceptions’] = True
+
+data = pd.read_csv("1.csv")
+
+
+
+app.layout = html.Div(children=(
+    dcc.Upload(
+        id='upload-data',
+        children=html.Div([
+            'Drag and Drop or ',
+            html.A('Select Files')
+        ]),
+        style={
+            'width': '50%',
+            'height': '60px',
+            'lineHeight': '60px',
+            'borderWidth': '1px',
+            'borderStyle': 'dashed',
+            'borderRadius': '5px',
+            'textAlign': 'center',
+            'margin': '10px'
+        },
+        # Allow multiple files to be uploaded
+        multiple=True
+    ),
+    daq.ToggleSwitch(
+        id='daq-light-dark-theme',
+        label=['min', 'max'],
+        style={'width': '250px', 'margin': 'auto'},
+        value=False
+    ),
+    html.Div(id='asdddd', children="min"),
+    dcc.Dropdown(
+        id='model-selector',
+        value=""
+    ),
+    html.Div(id='output-data-pareto'),
+    html.Div(id='output-data-table'),
+    html.Div(id='output-data-upload'),
+    html.Div(id='output-data-graph'),
+
+    html.H1(
+        children='Hello Dash',
+        style={
+            'textAlign': 'center',
+            'color': colors['text']
+        }
+    ),
+    generate_graph(data),
+
+    html.Div(children='Dash: A web application framework for Python.', style={
+        'textAlign': 'center',
+        'color': colors['text']
+    })
+))
+
+pre_style = {
+    'whiteSpace': 'pre-wrap',
+    'wordBreak': 'break-all',
+    'whiteSpace': 'normal'
+}
+
+def parse_contents(contents, filename):
+    content_type, content_string = contents.split(',')
+    decoded = base64.b64decode(content_string)
+    try:
+        if 'csv' in filename:
+            # Assume that the user uploaded a CSV file
+            df = pd.read_csv(
+                io.StringIO(decoded.decode('utf-8')))
+        elif 'xls' in filename:
+            # Assume that the user uploaded an excel file
+            df = pd.read_excel(io.BytesIO(decoded))
+        elif 'txt' or 'tsv' in filename:
+            # Assume that the user upl, delimiter = r'\s+'oaded an excel file
+            df = pd.read_csv(
+                io.StringIO(decoded.decode('utf-8')), delimiter=r'\s+')
+    except Exception as e:
+        print(e)
+        return html.Div([
+            'There was an error processing this file.'
+        ])
+
+    #OutPareto = printPareto(Pareto)
+    return html.Div([
+        #generate_graph(df),
     ])
+
+
+#def createCallback(size):
+@app.callback(Output('model-selector', 'options'),
+              [Input({'type': 'Pareto', 'index': ALL}, 'children')])
+def chengeoption(a):
+    return [{'label': str(a[i][0]), 'value': str(a[i][0])} for i in range(len(a))]
 
 @app.callback(Output('output-data-upload', 'children'),
               [Input('upload-data', 'contents')],
@@ -192,53 +214,108 @@ def parse_contents(contents, filename):
 def update_output(list_of_contents, list_of_names):
     if list_of_contents is not None:
         children = [
-            parse_contents(c, n) for c, n in
+            parse_dataa(c, n) for c, n in
             zip(list_of_contents, list_of_names)]
-        return children
-
+        #return children
 
 def optimalPareto(df):
-    #dd = df.to_numpy()
-    #dd[np.lexsort(np.fliplr(dd).T)]
-    pareto = df[:]
-    c = [False]*len(df)
-    #maybe rewite with [x >= y for i,x in enumerate(a) for j,y in enumerate(a) if i != j]
-    for a in df:
-        for b in df:
-            ziplist = list(zip(a, b, c))
-            if (all(np.equal(x, y) for x, y, c in ziplist)):
-                continue
-            if (all((np.greater_equal(x, y) and c) or
-                    (np.less_equal(x ,y) and not c)
-                    for x, y, c in ziplist)):
-                #print(a,b,c)
-                pareto = np.delete(pareto,np.where(np.all(pareto==b,axis=1)),axis=0)
-    #print(f"Pareto: {pareto}")
-    # Very slow
-    # Difference between  df and pareto (df-pareto)
-    ds = np.array(list((set(map(tuple, df)).difference(set(map(tuple, pareto))))))
-    if len(ds):
-        Pareto.append(optimalPareto(ds))
-    return pareto
+    Pareto = []
+    while len(df)>0:
+        #dd = df.to_numpy()
+        #dd[np.lexsort(np.fliplr(dd).T)]
+        pareto = df[:]
+        c = [False,True]#[False]*len(df)
+        #maybe rewite with [x >= y for i,x in enumerate(a) for j,y in enumerate(a) if i != j]
+        for a in df:
+            for b in df:
+                ziplist = list(zip(a, b, c))
+                if (all(np.equal(x, y) for x, y, c in ziplist)):
+                    continue
+                if (all((np.greater_equal(x, y) and c) or
+                        (np.less_equal(x ,y) and not c)
+                        for x, y, c in ziplist)):
+                    #print(a,b,c)
+                    pareto = np.delete(pareto,np.where(np.all(pareto==b,axis=1)),axis=0)
+        #print(f"Pareto: {pareto}")
+        # Very slow
+        # Difference between  df and pareto (df-pareto)
+        df = np.array(list((set(map(tuple, df)).difference(set(map(tuple, pareto))))))
+        Pareto.append(pareto)
+        #optimalPareto(df)
+    return Pareto
     #return printPareto(pareto,index)
 
-def printPareto(pareto):
-    pareto.reverse()
-    for index in range(len(pareto)):
-        paretoListDiv.append(
-            html.Div([
-                str(f"Pareto {index+1}:")
-            ]))
-        #print(f"Pareto{index}: {pareto}")
-        for i in range(len(pareto[index])):
+#Out paretoList with data from uploaded files
+@app.callback(Output('output-data-pareto', 'children'),
+            [Input('upload-data', 'contents'),
+             Input('upload-data', 'filename')])
+def printPareto(contents, filename):
+    paretoListDiv = []
+    if contents:
+        contents = contents[0]
+        filename = filename[0]
+        pareto = parse_dataa(contents, filename)
+        #Pareto.clear()
+        #Pareto.append(optimalPareto(pareto.to_numpy()))
+        #Pareto.reverse()
+        Pareto = optimalPareto(pareto.to_numpy())
+        for index in range(len(Pareto)):
             paretoListDiv.append(
-                html.Div([
-                    f"{pareto[index][i]}"
-                ])
-            )
-            print(f"{pareto[index][i]}")
-        print()
+                html.Details(
+                    [
+                    html.Summary(
+                        f'Pareto{index+1}'
+                    ),
+                    html.Div(
+                        [f"{Pareto[index]}"],
+                        id={'type': 'Pareto','index': index},
+                        )
+                    ],
+                    open=False,
+                    title="Pareto" + str(index + 1),
+                ))
+            print(f"{Pareto[index]}")
+            print()
+    #createCallback(len(paretoListDiv))
     return html.Div(paretoListDiv)
+
+#Out table with data from uploaded files
+@app.callback(Output('output-data-table', 'children'),
+            [Input('upload-data', 'contents'),
+             Input('upload-data', 'filename')])
+def update_table(contents, filename):
+    table = []
+    if contents:
+        children = [
+            parse_dataa(c, n) for c, n in
+            zip(contents, filename)]
+        for i in range(len(children)):
+            table.append(generate_table(children[i]))
+    return table
+
+#Out graph with data from uploaded files
+@app.callback(Output('output-data-graph', 'children'),
+            [Input('upload-data', 'contents'),
+             Input('upload-data', 'filename')])
+def update_graph(contents, filename):
+    graph = []
+    if contents:
+        children = [
+            parse_dataa(c, n) for c, n in
+            zip(contents, filename)]
+        for i in range(len(children)):
+            graph.append(generate_graph(children[i]))
+    return graph
+
+
+
+@app.callback(Output('asdddd', 'children'),
+            [Input('daq-light-dark-theme', 'value')])
+def ass(v):
+    if not v:
+        return html.Div("min")
+
+    return html.Div("max")
 
 if __name__ == '__main__':
     app.run_server(debug=True)
